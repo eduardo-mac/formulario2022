c058f80d82503d33aade667edce08852ef37dc86
David Jefferson <davidjeffersonbx@gmail.com>
2022-09-05 12:24:53 -0300

    ajuste

---
+++ js/scritps.js
@@ -0,0 +1,79 @@
+function mostrarOcultarSenha(){
+    let senha=document.getElementById("senha");
+    if(senha.type=="password"){
+        senha.type="text";
+    }else{
+        senha.type="password"
+    }
+}
+$(document).ready(function () { 
+    var $seuCampoCpf = $("#cpf");
+    $seuCampoCpf.mask('000.000.000-00', {reverse: true});
+});
+$(document).ready(function () { 
+    var $seuCampocnpj = $("#cnpj");
+    $seuCampocnpj.mask('02.000.000/0000-00', {reverse: true});
+});
+// ASPA NÃO FICA EM SEU LUGAR
+$(document).ready(function () { 
+    var $seuCampotel = $("#tel");
+    $seuCampotel.mask("(00) 00000-0000", {reverse: true});
+});
+$(document).ready(function () { 
+    var $seuCampocep = $("#cep");
+    $seuCampocep.mask('00000-000', {reverse: true});
+});
+
+var inputNome = document.querySelector("#soletra");
+soletra.addEventListener("keypress", function(e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+  
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
+var letra = document.querySelector("#soletra02");
+soletra02.addEventListener("keypress", function(e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+  
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
+var letrasomente = document.querySelector("#soletra01");
+soletra01.addEventListener("keypress", function(e) {
+    var keyCode = (e.keyCode ? e.keyCode : e.which);
+  
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
+
+
+// JS-RADIO-PJ
+// var selecionar_pfpj= document.getElementById("selec_pj");
+
+// selecionar_pfpj.addEventListener("click", function(){
+//     let pj = document.querySelector("#pj");
+    
+//     if(pj.style.display === "none"){
+//         pj.style.display = "block"
+//     }else {
+//         pj.style.display = "none";
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+    
+
