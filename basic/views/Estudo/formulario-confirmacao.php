bd738ec171be22b39e2fe9b75b43b391458b6f7a
Eduardo <eduardosilvamacedo96@gmail.com>
2022-09-28 22:29:43 -0300

    yii

---
+++ basic/views/Estudo/formulario-confirmacao.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<p>Você inseriu as seguintes Informações no Formulário:</p>
+
+<ul>
+    <li><label>Nome</label>: </li>
+    <li><label>E-mail</label>: </li>
+    <li><label>Idade</label>: </li>
+</ul>
